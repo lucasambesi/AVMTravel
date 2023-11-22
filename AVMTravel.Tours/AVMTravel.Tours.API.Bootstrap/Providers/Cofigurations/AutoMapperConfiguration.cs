7316693d9d73676438cd942f868ed0d69f1133f1
Lucas Ambesi <lucasambesi@hotmail.com>
2023-11-22 19:08:35 -0300

    Add automapper config

---
+++ AVMTravel.Tours/AVMTravel.Tours.API.Bootstrap/Providers/Cofigurations/AutoMapperConfiguration.cs
@@ -0,0 +1,25 @@
+﻿
+using AutoMapper;
+using AVMTravel.Tours.API.Application.Mappings;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AVMTravel.Tours.API.Bootstrap.Providers.Cofigurations
+{
+    [ExcludeFromCodeCoverage]
+    public static class AutoMapperConfiguration
+    {
+        public static IServiceCollection AddMapper(this IServiceCollection services)
+        {
+            MapperConfiguration mappingConfig = new(mc =>
+            {
+                mc.AddProfile(new LocationMappings());
+            });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+            return services;
+        }
+    }
+}
