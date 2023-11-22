f6aa683851e01a66c6bcb0e75d3eae1b3d32b066
Lucas Ambesi <lucasambesi@hotmail.com>
2023-11-22 19:15:52 -0300

    Add location mapper

---
+++ AVMTravel.Tours/AVMTravel.Tours.API.Application/Mappings/LocationMappings.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using AVMTravel.Tours.API.Application.UseCases.Locations.V1.GetById;
+using AVMTravel.Tours.API.Domain.DTOs;
+using AVMTravel.Tours.API.Domain.Entities;
+
+namespace AVMTravel.Tours.API.Application.Mappings
+{
+    public class LocationMappings : Profile
+    {
+        public LocationMappings()
+        {
+            #region Entities
+                CreateMap<LocationDto, Location>();
+            #endregion
+
+            #region Dtos
+                CreateMap<Location, LocationDto>();
+            #endregion
+
+            #region Results
+                CreateMap<LocationDto, GetByIdLocationResult>();
+            #endregion
+        }
+    }
+}
