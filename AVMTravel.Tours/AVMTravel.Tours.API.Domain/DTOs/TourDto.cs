736ca9ae963fce44601913cf3a428ec9226b6e53
Lucas Ambesi <lucasambesi@hotmail.com>
2023-11-23 01:14:31 -0300

    Add TourDto

---
+++ AVMTravel.Tours/AVMTravel.Tours.API.Domain/DTOs/TourDto.cs
@@ -0,0 +1,28 @@
+﻿using AVMTravel.Tours.API.Domain.Entities.Enums;
+using AVMTravel.Tours.API.Domain.Entities;
+
+namespace AVMTravel.Tours.API.Domain.DTOs
+{
+    public class TourDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public int DurationHours { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int LocationId { get; set; }
+
+        public LocationDto? LocationDto { get; set; }
+
+        public string? TourGuide { get; set; }
+
+        public EDifficultyLevelType DifficultyLevel { get; set; }
+    }
+}
