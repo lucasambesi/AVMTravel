aa2fe701f914411fa8220b0a0d3e20730a872b9c
Lucas Ambesi <lucasambesi@hotmail.com>
2023-11-23 01:01:17 -0300

    Add Location Repository

---
+++ AVMTravel.Tours/AVMTravel.Tours.API.Persistence/Percistence/Command/LocationRepository.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using AVMTravel.Tours.API.Domain.Entities;
+using AVMTravel.Tours.API.Domain.Interfaces.Commands;
+using AVMTravel.Tours.API.Persistence.Contexts;
+
+namespace AVMTravel.Tours.API.Persistence.Percistence.Command
+{
+    public class LocationRepository : ILocationRepository
+    {
+        private readonly BaseContext _dbContext;
+
+        private readonly IMapper _mapper;
+
+        public LocationRepository(
+            BaseContext dbContext,
+            IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> InsertAsync(Location location)
+        {
+            _dbContext.Locations.Add(location);
+
+            var result = await _dbContext.SaveChangesAsync();
+
+            return result > 0;
+        }
+    }
+}
